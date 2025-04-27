d75ac429d13ba7452a781997529b23aa4c7b77b0
Zeljko Ivanovic <zeljkoivanovic51@yahoo.com>
2025-04-27 19:16:46 +0200

    Prepare for deployment

---
+++ data/index.ts
@@ -0,0 +1,133 @@
+export const navItems = [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I believe great results come from clear communication and collaboration",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My technical skills",
+    description: "I'm always focused on improving",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech lover, dedicated to mastering development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building my own WordPress plugin.",
+    description: "Behind the Scenes",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60 opacity-50",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/5.jpg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Dreaming of a new project? Let's get started!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "My new portfolio website",
+    des: "A modern and responsive portfolio website showcasing my development work and technical expertise.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/next.svg"],
+    link: "https://github.com/ivanoviczeljko/portfolio/",
+  },
+  {
+    id: 2,
+    title: "Easy reCaptcha Shortcodes - WordPress plugin",
+    des: "Easy reCaptcha Shortcodes lets you add reCAPTCHA v2 via shortcodes, while reCAPTCHA v3 runs automatically, protecting your site from spam and bots.",
+    img: "/p2.png",
+    iconLists: ["/wp.svg", "/js.svg", "/css.svg", "/html.svg", "/svn.png"],
+    link: "https://wordpress.org/plugins/easy-recaptcha-shortcodes/",
+  }
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "WordPress Developer 3.2",
+    desc: "Develop and troubleshoot websites based on client requests, optimizing as needed",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Technical Website Builder III",
+    desc: "Actively contributing to new projects, including the revamping of team procedures, documentation, and the development of new websites",
+    className: "md:col-span-2", 
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Technical Website Builder II",
+    desc: "Taking ownership of problematic tasks and escalations with a focus on customer satisfaction and best judgment.",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Freelance Web Developer - Pharmacy App",
+    desc: "actively contributed to a significant project focused on developing an application dedicated to managing employee information and medication orders.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ivanoviczeljko/"
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/zivanovic1235/"
+  },
+];

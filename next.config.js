b4d86b5da2dec82b9de659b1d1e94c6905b42e7f
Zeljko Ivanovic <zeljkoivanovic51@yahoo.com>
2025-04-27 19:43:11 +0200

    Add new changes for the site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
-    output: 'export',
-  }
+  output: 'export', // Required for static export
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}

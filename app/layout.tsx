f1f6d2bdf208c2407749cd796f8af5b92dc1dbd7
Zeljko Ivanovic <zeljkoivanovic51@yahoo.com>
2025-04-28 00:19:11 +0200

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zeljkos's Portfolio",
+  title: "Zeljko's Portfolio",
   description: "Modern & Minimal Portfolio",
 };
 

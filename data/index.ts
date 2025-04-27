8b3fca56d82f47bf7ea48b58cb1fb82f4c3a2af3
Zeljko Ivanovic <zeljkoivanovic51@yahoo.com>
2025-04-27 21:17:38 +0200

    Added relative path to the images

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/1.jpg",
+    img: "./1.jpg",
     spareImg: "",
   },
   {
@@ -43,8 +43,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
   },
 
   {
@@ -54,8 +54,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60 opacity-50",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/5.jpg",
-    spareImg: "/grid.svg",
+    img: "./5.jpg",
+    spareImg: "./grid.svg",
   },
   {
     id: 6,
@@ -74,16 +74,16 @@ export const projects = [
     id: 1,
     title: "My new portfolio website",
     des: "A modern and responsive portfolio website showcasing my development work and technical expertise.",
-    img: "/p1.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/next.svg"],
+    img: "./p1.png",
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./next.svg"],
     link: "https://github.com/ivanoviczeljko/portfolio/",
   },
   {
     id: 2,
     title: "Easy reCaptcha Shortcodes - WordPress plugin",
     des: "Easy reCaptcha Shortcodes lets you add reCAPTCHA v2 via shortcodes, while reCAPTCHA v3 runs automatically, protecting your site from spam and bots.",
-    img: "/p2.png",
-    iconLists: ["/wp.svg", "/js.svg", "/css.svg", "/html.svg", "/svn.png"],
+    img: "./p2.png",
+    iconLists: ["./wp.svg", "./js.svg", "./css.svg", "./html.svg", "./svn.png"],
     link: "https://wordpress.org/plugins/easy-recaptcha-shortcodes/",
   }
 ];
@@ -94,40 +94,40 @@ export const workExperience = [
     title: "WordPress Developer 3.2",
     desc: "Develop and troubleshoot websites based on client requests, optimizing as needed",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Technical Website Builder III",
     desc: "Actively contributing to new projects, including the revamping of team procedures, documentation, and the development of new websites",
     className: "md:col-span-2", 
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
   {
     id: 3,
     title: "Technical Website Builder II",
     desc: "Taking ownership of problematic tasks and escalations with a focus on customer satisfaction and best judgment.",
     className: "md:col-span-2", 
-    thumbnail: "/exp3.svg",
+    thumbnail: "./exp3.svg",
   },
   {
     id: 4,
     title: "Freelance Web Developer - Pharmacy App",
     desc: "actively contributed to a significant project focused on developing an application dedicated to managing employee information and medication orders.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "./exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     link: "https://github.com/ivanoviczeljko/"
   },
   {
     id: 2,
-    img: "/link.svg",
+    img: "./link.svg",
     link: "https://www.linkedin.com/in/zivanovic1235/"
   },
 ];

9f59e73fe5bb2435c1a75c4e2d09f51bc90a2dbd
Zeljko Ivanovic <zeljkoivanovic51@yahoo.com>
2025-06-20 10:34:40 +0200

    Changed image and reverted link to the homepage

--- data/index.ts
+++ data/index.ts
@@ -90,9 +90,9 @@ export const projects = [
     id: 3,
     title: "OpenAPI, Swagger and Python (work in progress)",
     des: "A REST API project built with Python using the OpenAPI specification and Swagger UI for interactive documentation, ideal for showcasing clean and well-documented backend development skills.",
-    img: "./p3.png",
+    img: "./p3-1.png",
     iconLists: ["./python.svg", "./swagger.svg", "./flask.svg", "./openapi.svg"],
-    link: "Work in progress",
+    link: "#",
   }
 ];
 
